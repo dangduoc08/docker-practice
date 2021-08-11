36da3e792872de610b494a8530f65559f5e8194e
Đăng Được <duoc200894@gmail.com>
2021-08-12 01:02:06 +0700

    Add Rust has 3 services to write Dockerfile and docker-compose

---
+++ src/config.rs
@@ -0,0 +1,7 @@
+pub const POSTGRE_HOST: &str = "localhost";
+pub const POSTGRE_USER: &str = "postgres";
+pub const POSTGRE_PASS: &str = "test1234";
+
+pub const REDIS_HOST: &str = "localhost";
+
+pub const RABBITMQ_HOST: &str = "localhost";
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use amiquip::Connection;
+use docker_practice::config;
+use postgres::{Client as PostgresClient, NoTls};
+use redis::{Client as RedisClient, ConnectionLike};
+
+fn main() {
+  let psql_conn: String = format!(
+    "host={} user={} password={}",
+    config::POSTGRE_HOST,
+    config::POSTGRE_USER,
+    config::POSTGRE_PASS
+  );
+  let psql_client = match PostgresClient::connect(psql_conn.as_str(), NoTls) {
+    Ok(c) => c,
+    Err(e) => {
+      println!("PostgreSQL connect failed!");
+      panic!("{}", e)
+    }
+  };
+  if !psql_client.is_closed() {
+    println!("PostgreSQL has connected!");
+  }
+
+  let redis_url: String = format!("redis://{}", config::REDIS_HOST);
+  match RedisClient::open(redis_url) {
+    Ok(mut c) => {
+      if c.check_connection() {
+        println!("Redis has connected!");
+      } else {
+        println!("Redis connect failed!");
+        panic!()
+      }
+    }
+    Err(e) => panic!("{}", e),
+  };
+
+  let rabbitmq_url: String = format!("amqp://guest:guest@{}:5672", config::RABBITMQ_HOST);
+  match Connection::insecure_open(rabbitmq_url.as_str()) {
+    Ok(_conn) => {
+      println!("RabbitMQ has connected!");
+    }
+    Err(e) => {
+      println!("RabbitMQ connect failed!");
+      panic!("{}", e)
+    }
+  }
+}
